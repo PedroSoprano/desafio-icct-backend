fbf5e572a2b27f37cd5a8a892744a06c7e89c380
Pedro Soprano <pedro.soprano@grupoicts.com.br>
2024-01-11 11:58:28 -0400

    feature: adicionando middleware de erros

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bookRoutes from './routes/books.routes.js';
-
-import { AppError } from './errors/appError.js';
+import errorHandlerMiddleware from './middlewares/Error/errorHandling.middleware.js';
 
 dotenv.config();
 
@@ -10,26 +9,11 @@ const app = express()
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(errorHandlerMiddleware);
 
 app.use(bookRoutes)
-
-app.use((err, request, response, _) => {
-
-    if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-            status: 'error',
-            message: err.message,
-        });
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
-        status: 'error',
-        message: 'Internal server error',
-    });
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
