9c171a7f4804b7259dd3bbefc26e44cf5e042b2f
Pedro Soprano <pedro.soprano@grupoicts.com.br>
2024-01-11 13:23:14 -0400

    feature: adicionando rota de  atualização dos livros

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { createBookPayloadValidator } from '../validators/books/createBookPayload.validator.js'
+
 const routes = Router()
 
 import createBooksController from '../controllers/books/createBooks.controller.js'
 import listAllBooksController from '../controllers/books/listAllBooks.controller.js'
 import deleteBooksController from '../controllers/books/deleteBooks.controller.js'
+import updateBooksController from '../controllers/books/updateBooks.controller.js'
 
 routes.post('/livros', createBookPayloadValidator, createBooksController)
 routes.get('/livros', listAllBooksController)
 routes.delete('/livros/:id', deleteBooksController)
+routes.patch('/livros/:id', updateBooksController)
 
 export default routes
