44c34546a696a54a0c40bf9c1010f7a778d4c0f0
Pedro Soprano <pedro.soprano@grupoicts.com.br>
2024-01-11 11:58:07 -0400

    feature: criando middleware para tratamento de erros

---
+++ src/middlewares/Error/errorHandling.middleware.js
@@ -0,0 +1,19 @@
+import { AppError } from "../../errors/appError.js";
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+};
+
+export default errorHandlerMiddleware;
